2a1f863054ed53190667cec7cce6da3a6b4d6b74
Aimer <aimer.neige.soft@gmail.com>
2022-07-06 19:10:42 +0800

    update

--- src/app.js
+++ src/app.js
@@ -2,17 +2,9 @@
 const express = require("express");
 const mqtt = require("mqtt");
 const Redis = require("redis");
+
 // config
 const config = require("./config/config.json");
-
-// redis client
-const redisClient = Redis.createClient();
-redisClient.on("error", (err) => {
-  console.log("Redis Client Error", err);
-  process.exit(1);
-});
-redisClient.connect();
-
 // redis config
 const MAX_CACHE_SIZE = config.redis.max_cache_size;
 // mqtt config
@@ -24,6 +16,41 @@ const MQTT_CLIENT_ID = config.mqtt.client_id;
 const MQTT_QOS = config.mqtt.qos;
 // express config
 const EXPRESS_PORT = config.express.port;
+
+// redis client
+const redisClient = Redis.createClient();
+redisClient.on("error", (err) => {
+  console.log("Redis Client Error", err);
+  process.exit(1);
+});
+redisClient.connect();
+
+async function getNextId() {
+  const redisKey = MQTT_TOPIC + "/nextid";
+  let result = await redisClient.get(redisKey);
+  if (result == null) {
+    console.log(`Key '${redisKey}' not exist`);
+    redisClient.set(redisKey, 1);
+    return 1;
+  } else {
+    let value = parseInt(result);
+    value++;
+    redisClient.set(redisKey, value);
+    return value;
+  }
+}
+
+async function getDeviceInfo(deviceId) {
+  const redisKey = MQTT_TOPIC + "/device"
+  let redisData = await redisClient.lRange(redisKey, 0, -1);
+  let deviceItems = JSON.parse("[" + redisData + "]");
+  deviceItems.forEach((v) => {
+    if (v.id == deviceId) {
+      return v;
+    }
+  });
+  return null;
+}
 
 // mqtt client
 const client = mqtt.connect(MQTT_WS_URL, {
@@ -57,11 +84,12 @@ client.on("message", async (topic, message) => {
     });
   console.log(record);
   // save to redis
-  redisClient.lPush(topic, JSON.stringify(record));
+  redisKey = MQTT_TOPIC + "/data";
+  redisClient.lPush(redisKey, JSON.stringify(record));
   // trim redis when it is too large
-  size = await redisClient.lLen(topic);
+  size = await redisClient.lLen(redisKey);
   if (size > MAX_CACHE_SIZE) {
-    redisClient.lTrim(topic, 0, MAX_CACHE_SIZE - 1);
+    redisClient.lTrim(redisKey, 0, MAX_CACHE_SIZE - 1);
   }
 });
 
@@ -99,17 +127,58 @@ app.get("/", (req, res) => {
 app.get("/data", async (req, res) => {
   res.type("application/json");
   res.status(200);
-  redisData = await redisClient.lRange(MQTT_TOPIC, 0, -1);
+  let redisKey = MQTT_TOPIC + "/data";
+  let redisData = await redisClient.lRange(redisKey, 0, -1);
+  let dataObj = JSON.parse("[" + redisData + "]");
+  let deviceList = [];
+  dataObj.forEach((v) => {
+    deviceInfo = getDeviceInfo(v.id);
+    if deviceInfo != null {
+      deviceList.append({
+        time: v.time,
+        id: deviceInfo.id,
+        type: deviceInfo.type,
+        name: deviceInfo.name,
+        childType: deviceInfo.childType,
+      });
+    }
+  });
   res.send({
     status: 200,
     message: "success",
-    data: JSON.parse("[" + redisData + "]"),
+    data: deviceList,
   });
+});
+
+// add device
+app.post("/device", async (req, res) => {
+  res.type("application/json");
+  let deviceType = req.body.type;
+  let deviceName = req.body.name;
+  let deviceChildType = req.body.childType;
+  if deviceType === undefined || deviceName === undefined || deviceChildType === undefined {
+    res.status(400);
+    res.send({
+      status: 400,
+      message: "bad request",
+      data: null,
+    })
+  } else {
+    res.status(200);
+    redisKey = MQTT_TOPIC + "/device";
+    device = {
+      id: await getNextId(),
+      type: deviceType,
+      name: deviceName,
+      childType: deviceChildType,
+    }
+    redisClient.lPush(redisKey, JSON.stringify(device));
+  }
 });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  var err = new Error("Not Found");
+  let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
